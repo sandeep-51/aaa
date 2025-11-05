795c93336d941e495d4aed87e0e05943a05f202e
sandeep-51 <sandeepnath2063@gmail.com>
2025-11-05 18:47:01 +0530

    Delete clubconnect directory

--- clubconnect/clubs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Club, Event, Membership, Message, Announcement
-
-admin.site.register(Club)
-admin.site.register(Event)
-admin.site.register(Membership)
-admin.site.register(Message)
-admin.site.register(Announcement)
--- clubconnect/sitetheme/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class ThemeSettings(models.Model):
-    primary_color = models.CharField(max_length=7, default='#007bff')
-    secondary_color = models.CharField(max_length=7, default='#6c757d')
-
-    def __str__(self):
-        return 'Theme Settings'
